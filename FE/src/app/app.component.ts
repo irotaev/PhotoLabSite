331d4060369477315cfae9b70d145e78ef328052
Andrey <irotaev@gmail.com>
2017-03-12 13:11:45 +0300

    Верстка

--- FE/src/app/app.component.ts
+++ FE/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+
+declare var jQuery:any;
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit():void {
+      jQuery("#photo-gallery").unitegallery({
+        tiles_space_between_cols: 10,
+
+        tile_enable_border: true,
+        tile_border_width:8,
+        tile_border_color: "#0D7899"
+      });
+  }
 }
